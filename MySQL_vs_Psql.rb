78a7b88767d1ce1356d97ccbaabcb0177617cd29
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-09 11:25:22 +0530

    09 March

---
+++ MySQL_vs_Psql.rb
@@ -0,0 +1,4 @@
+# Difference between MySQL and PostgreSQL
+
+MySQL and PostgreSQL are both popular relational database management systems (RDBMS), but they have some differences in terms of features, performance, 
+and compatibility like postgres supports more data types like Hstore, JSON, and Array. PostgreSQL can handle complex query, and large data set more efficiently.
