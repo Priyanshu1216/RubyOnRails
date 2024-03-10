bb80e10e91d2e19e9bb63d80e3593be2409e2e03
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-10 10:43:20 +0530

    10 March

--- deadlock.rb
+++ deadlock.rb
@@ -1,6 +1,6 @@
 # Deadlock
 
-Deadlock is a situation in concurrent programming when two or more threads or processes are unable to preoceed because
+Deadlock is a situation in concurrent programming when two or more threads or processes are unable to proceed because
 each one is waiting for the other to release a resource or take a specific ation.
 As a result, the processes or threads remain in the blocked state indefinitely causing the system to freeze or become irreponsive.
 
