6f0b4db088614dc967150e0ea44a3adc65bc2eec
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-16 11:20:19 +0530

    16 March

---
+++ Anonymous_function.rb
@@ -0,0 +1,4 @@
+# Anonymous function  
+
+In Ruby, anonymous functions are known as lambdas or lambda functions.
+A lambda is a type of Proc object that can be defined inline, without being bound to a named variable or method.
