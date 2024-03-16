c05c346a2f75c9879d97639e410e16a6385c2998
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-16 11:22:25 +0530

    16 March(1)

---
+++ middleware.rb
@@ -0,0 +1,5 @@
+# Why do we use middleware?
+
+Middleware in Rails provides a powerful mechanism for modifying and extending the behavior of your application at various stages of the request/response cycle.
+It promotes modularity, reusability, and flexibility, allowing you to add custom functionality and integrate with other components or services.
+
