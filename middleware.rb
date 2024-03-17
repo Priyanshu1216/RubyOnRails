a4ac651da7d2152c8b7d0d10a477f3fac2bb85ef
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-03-17 22:56:48 +0530

    16 March

--- middleware.rb
+++ middleware.rb
@@ -2,4 +2,3 @@
 
 Middleware in Rails provides a powerful mechanism for modifying and extending the behavior of your application at various stages of the request/response cycle.
 It promotes modularity, reusability, and flexibility, allowing you to add custom functionality and integrate with other components or services.
-
