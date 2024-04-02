70b1049f2b140864e453d4da2541efb66ec9c757
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-02 23:39:28 +0530

    02 April Eagerloading vs Preloading

---
+++ eagerload_vs_preload.rb
@@ -0,0 +1,26 @@
+In the context of Ruby on Rails and ActiveRecord, "eager loading" and "preloading" are both techniques used to optimize database queries by reducing the number of database calls 
+made during the retrieval of associated records. However, they operate slightly differently:
+
+Eager Loading:
+
+Eager loading refers to the practice of loading associated records along with the main record in a single query. 
+It is achieved using the includes method in ActiveRecord queries. When you eager load associations, ActiveRecord generates a single SQL query that fetches both the main record and its associated records in one go, reducing the number of database queries required.
+
+Example:
+
+Copy code
+# Eager loading using includes
+@posts = Post.includes(:comments)
+In this example, @posts will contain a collection of Post objects, and each Post object will have its associated Comment objects loaded eagerly.
+
+Preloading:
+
+Preloading, on the other hand, is similar to eager loading but operates at a slightly lower level. It loads the associated records separately from the main query but does so more efficiently than lazy loading. Preloading is useful when you need to access the associated records individually but still want to minimize the number of database queries.
+
+Example:
+
+ruby
+Copy code
+# Preloading using preload
+@posts = Post.all.preload(:comments)
+In this example, @posts will contain a collection of Post objects, but the associated Comment objects won't be loaded immediately. However, when you access the comments association of each Post object, ActiveRecord will have already fetched all the associated Comment records in advance, reducing the need for additional queries.
