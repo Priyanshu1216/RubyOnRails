f9c17724f7a04d474339de74564f533612b02e6d
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-02 23:47:12 +0530

    02 April Lazy loading

--- eagerload_vs_preload.rb
+++ eagerload_vs_preload.rb
@@ -29,3 +29,36 @@ Example:
 
 In this example, @posts will contain a collection of Post objects, but the associated Comment objects won't be loaded immediately.
 However, when we access the comments association of each Post object, ActiveRecord will have already fetched all the associated Comment records in advance, reducing the need for additional queries.
+
+
+
+
+Lazy loading:
+
+In Ruby on Rails and ActiveRecord, lazy loading refers to the practice of deferring the loading of associated records until they are actually accessed or needed. 
+Unlike eager loading, where associated records are fetched in advance along with the main record, lazy loading loads associated records only when they are explicitly requested. 
+This helps to minimize unnecessary database queries and optimize performance.
+
+Here is a simple example to illustrate lazy loading:
+
+Let's consider the same Post and Comment models as before, where Post has many Comments.
+
+Example Models:
+
+class Post < ApplicationRecord
+  has_many :comments
+end
+
+# app/models/comment.rb
+class Comment < ApplicationRecord
+  belongs_to :post
+end
+Lazy Loading Example:
+ruby
+Copy code
+# Fetch a single post
+@post = Post.first
+
+# Access associated comments
+@comments = @post.comments
+In this example, when you fetch the @post object using Post.first, ActiveRecord executes a SQL query to retrieve the first post from the database. However, the associated comments are not fetched immediately. Instead, when you later access the @post.comments association, ActiveRecord will execute another SQL query to fetch the comments related to that specific post. This behavior is known as lazy loading because the associated records are loaded only when they are needed, not upfront.
