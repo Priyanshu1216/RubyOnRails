dbb7eee95aab877dee8677bd167ca9587dce2f39
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-08 23:14:34 +0530

    08 April(7)

--- put_vs_patch.rb
+++ put_vs_patch.rb
@@ -1,11 +1,11 @@
-PUT
+PUT-
 
 The put method is used to update the entire resource,meaning that the entire resource is replaced with the new data.
 It means that If you use put to update the resource, you need to pass the entire resource even if you only want to update a small part of resource.
 If you do not send the entitre resource, the parts that are not included will be overwritten with null or default values.
 
 
-PATCH
+PATCH-
 
 The patch method is used to update a part of resource.
 this means you only need to send the data that needs to be updated rather than entire resource.
