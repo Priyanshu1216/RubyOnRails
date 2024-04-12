a038c3f699f029f440f8648da9ca5372f8aa55e0
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-12 22:14:58 +0530

    12 April Hash practice(Question)

---
+++ Assignments/hash_practice.rb
@@ -0,0 +1,8 @@
+# Suppose you have a hash h = { a:1, b:2, c:3, d:4, e:6 }
+#     1. Get the value of key `:b`.
+#     2. Add to this hash the key:value pair `{f:5}`
+#     3. Remove all key:value pairs whose value is less than 5.5
+
+
+
+
