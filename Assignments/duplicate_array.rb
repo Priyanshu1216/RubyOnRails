d8c105b351a6ebe68f7400809f5289bae81835d5
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-12 23:33:53 +0530

    12 April Duplicate Array(Solution)

--- Assignments/duplicate_array.rb
+++ Assignments/duplicate_array.rb
@@ -1,2 +1,22 @@
 # Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
 # Find Duplicate in an array.
+
+
+arr = [1, 2, 3, 4, 5]
+k = 7
+
+def find_pairs(arr,k)
+  count  = 0
+  for i in 0...arr.length do
+    for j in (i+1)...arr.length do
+      # puts arr[i]
+      # puts arr[j]
+      if arr[i] + arr[j] == k
+        count += 1
+      end
+    end
+  end
+  count
+end
+
+puts find_pairs(arr,k)
