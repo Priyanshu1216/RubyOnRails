4081b90606bca507598bde966996b81a2b57ffa5
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-14 14:07:16 +0530

    12 April String Array(Solution)

--- Assignments/string_array_practice.rb
+++ Assignments/string_array_practice.rb
@@ -3,3 +3,21 @@
 # STRING TAKEN : "bug"
 # OUTPUT LIST:["bug bun bug bun bug bug","buggy bug bug buggy","bunny bug","no bun"]
 
+
+def rearrange_array(arr, str)
+  # Split each string into words
+  arr_words = arr.map { |s| s.split }
+  # p arr_words
+
+  # Sort the array based on the count of occurrences of the input string
+  sorted_arr = arr_words.sort_by { |words| -words.count(str) }
+
+  sorted_arr.map { |words| words.join(' ') }
+end
+
+arr = ["no bun", "bug bun bug bun bug bug", "bunny bug", "buggy bug bug buggy"]
+puts "Enter the string: "
+str = gets.chomp
+
+output_list = rearrange_array(arr, str)
+puts "OUTPUT LIST: #{output_list}"
