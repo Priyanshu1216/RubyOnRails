10965825cfee4d650e9e28044a3a3d775a9bdd0c
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-14 13:42:55 +0530

    12 April String Array(Question)

---
+++ Assignments/string_array_practice.rb
@@ -0,0 +1,5 @@
+# Take an array containing only strings. Now, take a string input from user and rearrange the elements of the list according to the number of occurence of the string taken from user in the elements of the list.
+# E.g.-LIST : ["no bun","bug bun bug bun bug bug","bunny bug","buggy bug bug buggy"]
+# STRING TAKEN : "bug"
+# OUTPUT LIST:["bug bun bug bun bug bug","buggy bug bug buggy","bunny bug","no bun"]
+
