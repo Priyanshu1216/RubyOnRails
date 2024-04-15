8e1a5f5ce43cdc7bace52cf1c6bf330b894623b8
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-04-15 22:44:54 +0530

    15 April(5)

--- Assignments/string_array_practice.rb
+++ Assignments/string_array_practice.rb
@@ -15,7 +15,6 @@ def rearrange_array(arr, str)
   sorted_arr.map { |words| words.join(' ') }
 end
 
-
 arr = ["no bun", "bug bun bug bun bug bug", "bunny bug", "buggy bug bug buggy"]
 puts "Enter the string: "
 str = gets.chomp
